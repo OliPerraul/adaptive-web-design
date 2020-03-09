f681b94d5f061018b796029f3bc182924e7b72b8
yassir <yassirnaouli@gmail.com>
2020-03-09 18:36:52 -0400

    test11

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: ['babel-polyfill', './src/index.html'],
+    entry: ['@babel/polyfill',
+            './src/index.html'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: './bundle.html'
@@ -14,7 +16,11 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html'
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name]-[hash].css'
+          }),
+      
     ],
     module: {
         rules: [
@@ -24,7 +30,34 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
-            }
+            },
+            {
+                test: /\.(html)$/,
+                use: {
+                  loader: 'html-loader',
+                  options: {
+                    interpolate: true
+                  }
+                }
+              },
+              {
+                test: /\.css$/,
+                use: [
+                  {
+                    loader: MiniCssExtractPlugin.loader,
+                  },
+                  'css-loader'
+                ]
+              },
+              {
+                test: /\.(png|jpg|jpeg|gif|mp4|svg|ico)(\?.*$|$)/,
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'img/'
+                }
+              }
+        
         ]
     }
 };
